1342c916b263793f9aecf12695f081b0605176ed
Farel Harimawan <farel@farels-mbp>
2019-03-23 00:45:33 -0700

    Initial Commit

---
+++ tipcalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipcalculator
+//
+//  Created by Farel Harimawan on 3/23/19.
+//  Copyright © 2019 Farel Harimawan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
